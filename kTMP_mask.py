f30306186478136a9328e4561f45f33537f964da
Philipp Reber <98315359+PhilippReber@users.noreply.github.com>
2023-12-05 10:34:53 -0800

    Add files via upload

---
+++ kTMP_mask.py
@@ -0,0 +1,130 @@
+import numpy as np
+import sounddevice as sd
+from numpy.random import permutation
+from scipy.io import savemat
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import os.path as op
+
+class kTMP_mask:
+    """
+    Example
+    -------
+    
+    Mask = kTMP_mask()
+    modulators = [4, (17,28)]  # should be a list. if element of list is a tuple, will blend randomly across.
+    duration = 10
+
+    Mask.make_mask(modulators=modulators, duration=duration)
+    Mask.save(file_name='mask_20min')
+    Mask.plot()
+    Mask.play()
+
+    """
+
+    def __init__(self):
+        self.signal = 0
+        self.sampling_rate = 0
+        self.duration = 0
+        
+    def phase_continuous_signal(self, start_freq, end_freq, duration, sampling_rate):
+        times = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
+        frequencies = np.linspace(start_freq, end_freq, len(times))
+        phase = 2 * np.pi * np.cumsum(frequencies) / sampling_rate
+        signal = np.sin(phase)
+        
+        return signal
+
+    def make_modulator(self, fmin, fmax, sampling_rate, duration, transition_duration):
+        freqs = np.arange(fmin, fmax, 1)
+        modulator = np.array([])
+        n_times = int(duration * sampling_rate)
+        while len(modulator)/sampling_rate < duration:
+            freqs = permutation(freqs)
+            for ix in range(len(freqs) - 1):
+                start_freq = freqs[ix]
+                end_freq = freqs[ix + 1]
+                transition_signal = self.phase_continuous_signal(start_freq, end_freq, transition_duration, sampling_rate)
+                if len(modulator) > 0:
+                    if np.diff(modulator)[-1] < 0:
+                        transition_signal *= -1
+                modulator = np.append(modulator, transition_signal)
+        
+        return modulator[:n_times]
+    
+    def make_mask(self, modulators, duration, carriers=[3500, 7000, 10500, 14000], 
+                  carrier_amps=[1, 1, 2, 1], sampling_rate=36000, transition_duration=1,
+                  intercept=2):
+        """
+        Parameters
+        ----------
+        modulators : List of integers or tuples | None
+            List of frequencies that will be applied as amplitude modulation to the kHz carrier. 
+            If elements are tuples (fmin,fmax), the modulation will blend randomly across frequency in that range.
+            If None, no modulation will be applied.
+        duration : int
+            The total duration of the mask in seconds.
+        carriers : List of integers
+            The carrier of the mask.
+        carrier_amps : list of integers
+            The individual volume of each carrier.
+        sampling_rate : int
+            The sampling rate of the returned mask.
+        transition_duration : int
+            How long each single frequency should last, if modulators are blended across.
+        intercept : int
+            The intercept of the modulation 
+        """
+        
+        if not isinstance(modulators, (list, type(np.array))) and modulators is not None:
+            raise Exception('modulators argument has to be list or array')
+        if len(carriers) != len(carrier_amps):
+            raise Exception('carriers and carrier_amps must be of equal length')
+        
+        all_times = np.linspace(0, duration, int(duration * sampling_rate), endpoint=False)
+        signal = np.sum([amp * np.sin(2 * np.pi * carrier_freq * all_times) for carrier_freq, amp in zip(carriers, carrier_amps)], axis=0)
+
+        if modulators:
+            for f in modulators:
+                if isinstance(f, tuple):
+                    fmin, fmax = f
+                    modulator = self.make_modulator(fmin, fmax, sampling_rate, duration, transition_duration)
+                elif isinstance(f, int):
+                    modulator = np.sin(2 * np.pi * f * all_times)
+            
+                signal *= 0.5 * (intercept + modulator)
+
+        self.signal = signal
+        self.sampling_rate = sampling_rate
+        self.duration = duration
+
+    def plot(self):
+        # plot the signal
+        plt.plot(self.signal)
+        plt.show()
+        
+    def play(self):
+        # play the signal
+        sd.play(self.signal, samplerate=self.sampling_rate)
+        sd.wait()
+
+    def save(self, file_name):
+        # save to matlab and wav format
+        path = op.join(op.dirname(__file__), file_name)
+        mdict = {'signal': self.signal, 'sampling_rate': self.sampling_rate, 'duration': self.duration}
+        savemat(f'{path}.mat', mdict)
+        wavfile.write(f'{path}.wav', self.sampling_rate, self.signal)
+
+
+def main():
+    Mask = kTMP_mask()
+    modulators = [(2,4)]
+    duration = 10
+
+    Mask.make_mask(modulators=modulators, duration=duration)
+    # Mask.save('mask')
+    Mask.plot()
+    Mask.play()
+
+if __name__ == '__main__':
+    main()
